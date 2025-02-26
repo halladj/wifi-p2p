0efbe682218f6706c202c5c5d9b8fa670878d47d
halladj <halladj00@gmail.com>
2025-02-26 12:19:07 -1000

    added the correct network interface + proper steps order

--- wifi-p2p.sh
+++ wifi-p2p.sh
@@ -3,31 +3,39 @@
 INTERFACE="wlan0"
 
 # Start wpa_supplicant in background
-sudo wpa_supplicant -Dnl80211 -i $INTERFACE -c /dev/null -B
+# sudo wpa_supplicant -Dnl80211 -i $INTERFACE -c /dev/null -B
+
+### TODO: add the wps_pbc thingy
+#wpa_cli -i p2p-dev-wlan0 wps_pbc
 
 # Wait for initialization
+
 sleep 2
 
 # Start P2P listen mode
-sudo wpa_cli p2p_listen
+#sudo wpa_cli -i p2p-dev-wlan0 p2p_listen
+#wpa_cli -i p2p-dev-wlan0 p2p_listen
 
 # Loop to continuously scan and connect
 while true; do
+    wpa_cli -i p2p-dev-wlan0 wps_pbc
+
     echo "Scanning for peers..."
-    sudo wpa_cli p2p_find
+    wpa_cli -i p2p-dev-wlan0 p2p_find
     sleep 5  # Give some time to discover devices
 
-    PEERS=$(sudo wpa_cli p2p_peers)
+    PEERS=$(wpa_cli  -i p2p-dev-wlan0 p2p_peers)
     if [ -n "$PEERS" ]; then
         echo "Found peers: $PEERS"
         for PEER in $PEERS; do
             echo "Trying to connect to $PEER..."
-            sudo wpa_cli p2p_connect $PEER pbc persistent
+            wpa_cli  -i p2p-dev-wlan0 p2p_connect $PEER pbc persistent
         done
     else
         echo "No peers found. Scanning again..."
     fi
 
     sleep 10  # Wait before next scan
+    wpa_cli -i p2p-dev-wlan0 p2p_stop_find
 done
 
